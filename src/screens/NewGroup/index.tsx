63d0383203d84dd97fbf62a872b9423c2b0a41af
GuilhermeVolpato <volpato2103@gmail.com>
2023-02-10 14:38:39 -0300

    Projeto 2, modulo 4 asyncstorage 50% completo

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -5,14 +5,36 @@ import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
+import {AppError} from "@utils/AppError"
+import { groupCreate } from "@storage/group/groupCreate";
+import { Alert } from "react-native";
 
 export function NewGroup() {
   const[group, setGroup] = useState('')
 
   const navigation = useNavigation();
 
-  function handleNew(){
-    navigation.navigate('Players', {group});
+
+  async function handleNew(){
+    try{
+      if(group.trim().length === 0){
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma')
+      }
+
+      await groupCreate(group)
+      navigation.navigate('Players', {group});
+
+    }catch(error){
+
+      if(error instanceof AppError){
+        Alert.alert('Novo Grupo', error.message)
+      }else{
+        Alert.alert('Novo Grupo','Não foi possível criar uma nova turma')
+        console.log(error)
+      }
+      
+    }
+    
   }
 
   return (
